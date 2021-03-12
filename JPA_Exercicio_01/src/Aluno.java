3702eb9f70c9ba1e8221dfe40cd2ae745849c57e
Alécio <alecio.godoi@ymail.com>
2021-03-12 10:14:20 -0300

    JPA - Exercício 01

---
+++ JPA_Exercicio_01/src/Aluno.java
@@ -0,0 +1,69 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Aluno {
+	
+	// Chave Primaria RA
+	@Id
+	private String ra;
+	
+	private String nome;
+	private String email;
+	private String celular;
+	private double nota1;
+	private double nota2;
+	
+	public Aluno() {
+		super();
+	}
+	
+	public Aluno(String ra, String nome, String email, String celular, double nota1, double nota2) {
+		super();
+		this.ra = ra;
+		this.nome = nome;
+		this.email = email;
+		this.celular = celular;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}	
+}
---
+++ JPA_Exercicio_01/src/AlunoDAO.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class AlunoDAO {
+
+	public void insert(Aluno aluno) {
+		
+	}
+	
+	public void update(Aluno aluno) {
+		
+	}
+
+	public void delete(int id) {
+		
+	}
+
+	public Aluno find(int id) {
+		return null;
+	}
+	
+	public List<Aluno> getAlunos() {
+		return null;
+	}
+	
+}
