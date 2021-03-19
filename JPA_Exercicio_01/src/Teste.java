c1d9617974d2ffbf1c390e2065f4b50ecfdc888c
Alécio <alecio.godoi@ymail.com>
2021-03-19 11:04:01 -0300

    Aula 06 - Mapeamentos

--- JPA_Exercicio_01/src/Teste.java
+++ JPA_Exercicio_01/src/Teste.java
@@ -1,22 +1,54 @@
+import javax.swing.JOptionPane;
 
 public class Teste {
-
+	
+	static Aluno aluno;
+	
 	public static void main(String[] args) {
+		aluno = new Aluno();
 		AlunoDAO alunoDAO = new AlunoDAO();
-		//alunoDAO.insert(new Aluno("1234-X","Jose","jose@gmail.com", "(11)9999-8888", 5.5, 8));
-		//alunoDAO.insert(new Aluno("0987-Y","Ana","ana@gmail.com", "(11)7777-8888", 6, 8.5));
-		//alunoDAO.insert(new Aluno("0111-K","Pedro","pedro@gmail.com", "(11)6666-8888", 6, 8.5));
+		String menu = "0 - Sair\n"
+					+ "1 - Incluir\n"
+					+ "2 - Alterar\n"
+					+ "3 - Excluir\n"
+					+ "4 - Pesquisar por RA\n";
+		int opcao = 0;
+		do {
+			opcao = Integer.valueOf(JOptionPane.showInputDialog(menu));
+			switch (opcao) {
+			case 1:
+				entradaDados();
+				alunoDAO.insert(aluno);
+				break;
+			case 2:
+				entradaRA();
+				aluno = alunoDAO.find(aluno.getRa());
+				alunoDAO.update(aluno);
+				break;
+			case 3:
+				entradaRA();
+				alunoDAO.delete(aluno.getRa());
+				break;
+			case 4:
+				entradaRA();
+				aluno = alunoDAO.find(aluno.getRa());
+				JOptionPane.showMessageDialog(null, aluno);
+				break;	
+			}
+		} while (opcao!=0);
 
-		//System.out.println(alunoDAO.find("1234-X"));
-		//System.out.println(alunoDAO.find("0987-Y"));
-		//System.out.println(alunoDAO.find("0111-K"));
-		
-		alunoDAO.delete("0111-K");
-		alunoDAO.update(new Aluno("1234-X","Jose da Silva JR","jose.silva@gmail.com", "(11)9999-7799", 5.6, 8.2));
-		alunoDAO.updateQuery(new Aluno("0987-Y","Ana Maria Braga","ana.maria@gmail.com", "(11)7777-8888", 7.2, 7.5));
-
-		for (Aluno aluno: alunoDAO.getAlunos()) {
-			System.out.println(aluno);
-		}
+	}
+	
+	public static void entradaDados() {
+		entradaRA();
+		aluno.setNome(JOptionPane.showInputDialog("Nome"));
+		aluno.setEmail(JOptionPane.showInputDialog("Email"));
+		aluno.setCelular(JOptionPane.showInputDialog("Celular"));
+		aluno.setNota1(Double.valueOf(JOptionPane.showInputDialog("Nota 1")));
+		aluno.setNota2(Double.valueOf(JOptionPane.showInputDialog("Nota 2")));
+	}
+	
+	public static void entradaRA() {
+		aluno.setRa(JOptionPane.showInputDialog("RA"));
 	}
 }
